a5b8c6019337dc1937feca1b4ba5900b0409c113
snichita817 <nichita.sincarenco@gmail.com>
2023-03-06 20:33:43 +0200

    improved chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/models/transaction.dart';
+import 'package:personal_expenses/widgets/chart_bar.dart';
 import '../models/chartData.dart';
 
 class Chart extends StatelessWidget {
@@ -39,15 +40,31 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get weekSpending {
+    return groupedTransactionValues.fold(0.0, (previousValue, element) {
+      return previousValue += element.spentForTheDay;
+    });
+  }
+
   Widget build(BuildContext context) {
     print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((data) {
-          return Text("${data.dayOfTheWeek} : ${data.spentForTheDay}");
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: data.dayOfTheWeek, 
+                spentAmount: data.spentForTheDay, 
+                spentPercentage: weekSpending == 0.0 ? 0.0 : data.spentForTheDay/weekSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,56 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spentAmount;
+  final double spentPercentage;
+
+  ChartBar(
+      {required this.label,
+      required this.spentAmount,
+      required this.spentPercentage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // fitted box to shrink the text of the amount spent
+        FittedBox(child: Text("${spentAmount.toStringAsFixed(0)} lei")),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(children: <Widget>[
+            // element at the bottom
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            // element of the top of the stack
+            FractionallySizedBox(
+              heightFactor: spentPercentage,
+              // this child(container) will have the color i want for the graph
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            )
+          ]),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
